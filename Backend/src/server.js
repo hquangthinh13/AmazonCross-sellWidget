f5510ae1b259c52799cbfb9115d9230f30af4d15
hquangthinh13 <hquangthinh13@gmail.com>
2025-11-23 07:57:53 +0700

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const allowedOrigins = [
   "http://localhost:5173", // local dev
-  "https://amazon-cross-sell-widget.vercel.app/",
+  "https://amazon-cross-sell-widget.vercel.app",
 ];
 
 app.use(
